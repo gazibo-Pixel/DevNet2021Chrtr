eb665cf1e313a701f2fefb9093ed57c866de071b
Mohamed Taha <mohamedt88@protonmail.com>
2021-07-06 19:27:49 -0500

    This should go somewhere eh?

---
+++ compare.py
@@ -0,0 +1,7 @@
+def compi(n):
+	i = int(input("Please enter a value to compare it wiht 50, will give you a result fasho: "))
+	if i >50:
+		print("Boy o Boy {} is def grater than 50".format(i))
+	else:
+		print("SHEEEEET boy {} is less than 50".format(i))
+
